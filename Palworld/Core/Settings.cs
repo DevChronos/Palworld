17a503dac7386130968127a22a0e21c78992f554
Chronos <chronos.gamer@outlook.com>
2024-01-26 13:36:39 -0300

    Offset update

--- Palworld/Core/Settings.cs
+++ Palworld/Core/Settings.cs
@@ -6,13 +6,13 @@ namespace Palworld.Core
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "ImGui")]
     internal static class Settings
     {
-        //UWorld => 48 8B 05 ? ? ? ? 48 8B 88 ? ? ? ? 48 85 C9 74 06 48 8B 49 70
-        //GObjects => 89 0D ? ? ? ? 48 8B DF 48 89 5C 24
-        //GNames => 48 8D 0D ? ? ? ? E8 ? ? ? ? 4C 8B C0 C6 05
+        //UWorld => 48 8B 1D ?? ?? ?? ?? 48 85 DB 74 33 41 B0
+        //GObjects => 48 8B 05 ? ? ? ? 48 8B 0C C8 4C 8D 04 D1 EB 03
+        //GNames => 48 8D 05 ? ? ? ? EB 13 48 8D 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 0F 10
 
         public const string PROCESS_NAME = "Palworld-Win64-Shipping";
-        public const nint WORLD_OFFSET = 0x8924E28;
-        public const nint GNAMES_OFFSET = 0x8717A00;
+        public const nint WORLD_OFFSET = 0x890BD28;
+        public const nint GNAMES_OFFSET = 0x86FE900;
 
         public static bool EnablePlayer = Properties.User.Default.EnablePlayer;
         public static Vector4 PlayerColor = Properties.User.Default.PlayerColor.ToVector4();
