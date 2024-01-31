60191d2fc21ef0ad3936b46f36aee3849d1a4514
Chronos <chronos.gamer@outlook.com>
2024-01-31 20:38:28 -0300

    Item dictionary

--- Palworld/ESP/ItemEsp.cs
+++ Palworld/ESP/ItemEsp.cs
@@ -18,8 +18,25 @@ namespace Palworld.ESP
                 if (location.GetDistance() > Settings.ItemDistance || !location.ToScreen(out Vector2 screenPosition))
                     continue;
 
-                DrawCenteredText(screenPosition, Settings.ItemColor, $"{item.MapObjectModel.Name} [{location.GetDistance():0}m]");
+                var name = item.MapObjectModel.Name;
+
+                if (ItemNameDictionary.TryGetValue(name, out string custom))
+                    name = custom;
+
+                DrawCenteredText(screenPosition, Settings.ItemColor, $"{name} [{location.GetDistance():0}m]");
             }
         }
+
+        private static Dictionary<string, string> ItemNameDictionary = new Dictionary<string, string>()
+        {
+            { "DamagableRock0002", "Ore Mine" },
+            { "DamagableRock0004", "Coal Mine" },
+            { "DamagableRock0005", "Paladium Mine" },
+            { "DamagableRock0006", "Sulfur Mine" },
+            { "DamagableRock0009", "Stone Mine" },
+            { "DamagableRock0010", "Stone Mine" },
+            { "DamagableRock0011", "Stone Mine" },
+            { "DamagableRock0012", "Stone Mine" },
+        };
     }
 }
